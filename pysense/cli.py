068e2e1eb58df296a84312aa824fde17c3794d20
Andrew Sawyers <andrew@sawdog.com>
2018-10-22 21:34:38 -0700

    TG-6 #closed TG-7 #closed pysense-6 pysense-7
    Add more functionality to the sensecli:
    - added --version option, using versioneer to manage versions
      * setup.cfg and versioneer.py are part of the versioneer integration
    - add ability to call the cli without any commands, and just return
    
    currently working are the following apis within the CLI:
    - active_*: power, solar_power, voltage, frequency
    - devices --active/--in-active
    - production trends: daily, weekly, monthly, and yearly
      sensecli production --daily | sensecli production --yearly
    - usage trends: daily, weekly, monthly, and yearly
      sensecli usage --daily | sensecli usage --weekly
    
    - colorama added to begin work on using color within the cli

--- pysense/cli.py
+++ pysense/cli.py
@@ -1,10 +1,14 @@
 import click
 
 from pysense import api
+import versioneer
 
 
 class SenseCLI(object):
     """sense context and hanger of things cli-ish"""
+
+    usage = False
+    production = False
 
     def __init__(self):
         """set override for username and pass if provided"""
@@ -15,7 +19,19 @@ class SenseCLI(object):
 pass_sensecli = click.make_pass_decorator(SenseCLI, ensure=True)
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-@click.group(context_settings=CONTEXT_SETTINGS, chain=True)
+
+def print_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo(versioneer.get_version())
+    ctx.exit()
+
+
+@click.group(context_settings=CONTEXT_SETTINGS,
+             chain=True,
+             invoke_without_command=True)
+@click.option('--version', is_flag=True, callback=print_version,
+              expose_value=False, is_eager=True)
 @click.pass_context
 def cli(ctx):
     ctx.obj = SenseCLI()
@@ -36,13 +52,85 @@ def devices(sensecli, active):
     click.echo(devices)
 
 
-@cli.command('trend')
-@click.option('--period', type=click.Choice(['daily',
-                                             'weekly',
-                                             'monthly',
-                                             'yearly']))
-def trend(period):
-    click.echo('trend period %s' % trend)
+@cli.command('active_power')
+@click.pass_obj
+def active_power(sensecli):
+    """return the realtime current active power consumption
+    """
+    api = sensecli.api
+    click.echo(api.active_power)
+
+
+@cli.command('active_solar_power')
+@click.pass_obj
+def active_solar_power(sensecli):
+    """return the realtime current active solar power creation
+    """
+    api = sensecli.api
+    click.echo(api.active_solar_power)
+
+
+@cli.command('active_voltage')
+@click.pass_obj
+def active_voltage(sensecli):
+    """return the realtime current active solar voltage
+    """
+    api = sensecli.api
+    click.echo(api.active_voltage)
+
+
+@cli.command('active_frequency')
+@click.pass_obj
+def active_frequency(sensecli):
+    """return the realtime current active frequency
+    """
+    api = sensecli.api
+    click.echo(api.active_frequency)
+
+
+
+@cli.command('production')
+@click.option('--daily', 'period', flag_value='daily', default=True,
+              help='Pass --daily to display the daily utilization')
+@click.option('--weekly', 'period', flag_value='weekly',
+              help='Pass --weekly to display the weekly utilization')
+@click.option('--monthly', 'period', flag_value='monthly',
+              help='Pass --monthly to display the monthly utilization')
+@click.option('--yearly', 'period', flag_value='yearly',
+              help='Pass --yearly to display the yearly utilization')
+@click.pass_obj
+def production(sensecli, period):
+    if sensecli.usage:
+        raise 'Foo'
+    sensecli.production = True
+
+
+    attr = 'production'
+    api = sensecli.api
+    value = getattr(api, period + '_' + attr)
+    click.echo(value)
+
+
+@cli.command('usage')
+@click.option('--daily', 'period', flag_value='daily', default=True,
+              help='Pass --daily to display the daily consumption')
+@click.option('--weekly', 'period', flag_value='weekly',
+              help='Pass --weekly to display the weekly oconsumption')
+@click.option('--monthly', 'period', flag_value='monthly',
+              help='Pass --monthly to display the monthly')
+@click.option('--yearly', 'period', flag_value='yearly',
+              help='Pass --yearly to display the yearly consumption')
+@click.pass_obj
+def trend(sensecli, period):
+    """return the usage trend for the supplied period"""
+    if sensecli.usage:
+        raise 'Foo'
+    sensecli.production = True
+
+    attr = 'usage'
+    api = sensecli.api
+    value = getattr(api, period + '_' + attr)
+    click.echo(value)
 
 
 if __name__ == "__main__":
