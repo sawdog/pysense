74144b670577d99c0dc5f27226f797be775b28b8
Andrew Sawyers <andrew@sawdog.com>
2018-10-22 21:35:06 -0700

    TG-6 #closed TG-7 #closed pysense-6 pysense-7
    Add more functionality to the sensecli:
     * added --version option, using versioneer to manage versions
    
    currently working are the
    following apis within the CLI
    
    - active_*: power, solar_power, voltage, frequency
    - devices --active/--in-active
    -

--- setup.py
+++ setup.py
@@ -1,26 +1,39 @@
 from setuptools import setup
 
+import versioneer
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name = 'pysense',
-    packages = ['pysense'], 
+    name='pysense',
+    packages=['pysense'],
     install_requires=[
+        'arrow',
         'click',
+        'colorama',
         'confuse',
         'requests',
+        'versioneer',
         'websocket-client',
     ],
-    version = '0.7.0',
-    description = 'API for the Sense Energy Monitor, forked from the sense project by scottbonline/sense',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='API for the Sense Energy Monitor, forked from the sense '
+                'project by scottbonline/sense',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = 'Andrew Sawyers',
-    author_email = 'andrew@sawdog.com',
-    url = 'https://github.com/sawdog/sense',
-    keywords = ['sense', 'energy', 'monitot' 'api', 'pytest', 'python3',
-                'python2'],
+    author='Andrew Sawyers',
+    author_email='andrew@sawdog.com',
+    url='https://github.com/sawdog/pysense',
+    keywords=['sense',
+              'energy',
+              'monitor',
+              'api',
+              'pytest',
+              'python3',
+              'python2'],
     classifiers = [],
     entry_points='''
         [console_scripts]
